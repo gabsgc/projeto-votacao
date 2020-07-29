def77ba450002f43405f415f5a46c2e0c5178a17
gabsgc <gabriellagarccia@gmail.com>
2020-07-28 22:14:37 -0300

    Add modal

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ function votar(){
     
     if(valorVoto == 1){
         candidato1 = candidato1 + 1
-        alert("O seu voto no candidato Afonso foi computado com sucesso")
+        alert("O seu voto no candidato Afonso foi computado")
     }
     if(valorVoto == 2){
         candidato2 = candidato2 + 1
@@ -60,13 +60,13 @@ function votar(){
 }
  // nulo + branco * 100  / total
 
-function resultado(){
-    document.getElementById("resultado").innerHTML= "O candidato Afonso teve " + candidato1 + " votos";
-    document.getElementById("resultado1").innerHTML= "O candidato César teve " + candidato2 + " votos";
-    document.getElementById("resultado2").innerHTML= "A candidata Fernanda teve " + candidato3 + " votos";
-    document.getElementById("resultado3").innerHTML= "A candidata Mariana teve " + candidato4 + " votos";
-    document.getElementById("resultado4").innerHTML= "O total de votos nulos são " + nulo;
-    document.getElementById("resultado5").innerHTML= "O total de votos brancos são " + branco;
-    document.getElementById("resultado6").innerHTML= "O total de votos são " + totalVotos;
-    document.getElementById("resultado7").innerHTML= "O percentual de votos nulos e brancos é de " + porcentagem + "%";
+function resultado() {
+    document.getElementById("c1").innerHTML = candidato1;
+    document.getElementById("c2").innerHTML = candidato2;
+    document.getElementById("c3").innerHTML = candidato3;
+    document.getElementById("c4").innerHTML = candidato4;
+    document.getElementById("nulo").innerHTML = nulo;
+    document.getElementById("branco").innerHTML = branco;
+    document.getElementById("total").innerHTML = totalVotos;
+    document.getElementById("percentual").innerHTML = porcentagem + "%";
 }
